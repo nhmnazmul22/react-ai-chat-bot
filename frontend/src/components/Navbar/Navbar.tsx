4d57efce19ca53aead8e70768694deb1be4d9992
nhmnazmul22 <najmulmonir12345@gmail.com>
2025-09-30 23:37:04 +0600

    feat: split whole application within some component and create some folder for components

---
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Container from "../Layout/Container";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <Container>
+        <div></div>
+      </Container>
+    </nav>
+  );
+};
+
+export default Navbar;
